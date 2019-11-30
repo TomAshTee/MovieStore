bf4599c8bb0afb85de1ec916d0d9b50eb891dc19
Tomasz Jaeschke <jaeschke.tomasz@gmail.com>
2019-11-30 18:29:16 +0100

    Dodaj pliki projektów.

---
+++ MovieStore/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieStore.Models;
+using MovieStore.ViewModels;
+
+namespace MovieStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public List<Customer> customersList = new List<Customer>
+        {
+            new Customer {Name = "Thomas", Id = "1"},
+            new Customer {Name = "Alex", Id = "2"},
+            new Customer {Name = "Jane", Id = "3"}
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customersViewModel = new CustomerViewModel();
+            customersViewModel.Customers = customersList;
+
+            return View(customersViewModel);
+        }
+
+        public ActionResult Details(string id)
+        {
+            var customer = customersList.Find(x => x.Id == id);
+
+            return View(customer);
+        }
+    }
+}
